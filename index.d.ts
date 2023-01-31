df2128ce8f929c7716b68f75be895d7c6122b1ee
E0SelmY4V-MPC <2507917746@qq.com>
2023-02-01 00:06:25 +0800

    Add 导出

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,15 @@
+/**
+ * ESM Entry
+ * @version 0.9.0
+ * @license MIT
+ * @link https://github.com/E0SelmY4V/esm-entry
+ */
+declare module '.'
 export function getFilename(n: string): string
 export function getDirname(n: string): string
-type FnP = (...arg: [string, ...any[]]) => any
-type MixedFnP<P extends FnP> = { [I in keyof P]: P[I] } & ((n: string) => ReturnType<P>)
+export type FnP = (...arg: [string, ...any[]]) => any
+export type MixedFnP<P extends FnP> = { [I in keyof P]: P[I] } & ((n: string) => ReturnType<P>)
 export function fnFilename<P extends FnP>(p: P): MixedFnP<P>
 export function fnDirname<P extends FnP>(p: P): MixedFnP<P>
+import * as A from '.'
+export default A;
